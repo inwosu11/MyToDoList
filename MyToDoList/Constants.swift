09c4154a700a93d9bca00240f384e3680367b13a
IvanP4 <ivanpalmer717@gmail.com>
2024-04-23 08:06:34 -0700

    Creation of constants file

---
+++ MyToDoList/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MyToDoList
+//
+//  Created by user257944 on 4/23/24.
+//
+
+import Foundation
+struct Constants{
+    static let kSortField = "sortField"
+    static let kSortDirectionAscending = "sortDirectionAscending"
+}
