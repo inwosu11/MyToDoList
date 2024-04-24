5563442eeeb7d08e960550e337972ea927373754
fredy <freddito310@gmail.com>
2024-04-24 06:15:31 -0400

    texting

--- MyToDoList/TodoListTableViewController.swift
+++ MyToDoList/TodoListTableViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class TodoListTableViewController: UITableViewController {
+    
+    let Item = ["Jim", "John", "Dana", "Rosie", "Justin", "Jeremy", "Sarah", "Matt", "Joe", "Donald", "Jeff"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,24 +24,24 @@ class TodoListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return Item.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
 
         // Configure the cell...
-
-        return cell
+        cell.textLabel?.text = Item[indexPath.row]
+            return cell
+        
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
